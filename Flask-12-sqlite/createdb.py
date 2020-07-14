de2ef0c25a36282f40d477c362bbf3c8525097b5
xugaoxiang <xugx.ai@gmail.com>
2020-07-14 16:46:50 +0800

    delete flask-12

--- Flask-12-sqlite/createdb.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-con = sqlite3.connect("users.db")
-con.execute(
-    "create table user (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT UNIQUE NOT NULL)")
-
-print("Table created successfully!")
-con.close()
