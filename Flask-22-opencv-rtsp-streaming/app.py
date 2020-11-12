909d06985fe22356d6c8671a95d6b18f974f0e05
xugaoxiang <xugx.ai@gmail.com>
2020-11-12 11:12:20 +0800

    add flask-22-opencv-rtsp-streaming

---
+++ Flask-22-opencv-rtsp-streaming/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+
+# camera = cv2.VideoCapture('rtsp://admin:admin@172.21.182.12:554/cam/realmonitor?channel=1&subtype=1')
+camera = cv2.VideoCapture(0)
+
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
